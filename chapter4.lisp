11585d662b8ed1cafe64acc294db9be6d6c826b9
Anthony Gagliano <antogagliano3@gmail.com>
2023-08-29 23:20:16 -0400

    <2023-08-29> More Chapter 4 progress, worked on GPS version 2

--- chapter4.lisp
+++ chapter4.lisp
@@ -190,3 +190,89 @@
     (fresh-line *debug-io*)
     (dotimes (i indent) (princ " " *debug-io*))
     (apply #'format *debug-io* format-string args)))
+
+;; 4.11 GPS Version 2 : A More General Problem Solver
+
+(defun executing-p (x)
+  "Is x of the form: (executing ...) ?"
+  (starts-with x 'executing))
+
+(defun start-with (list x)
+  "Is this a list whose first element is x?"
+  (and (consp list) (eql (first list) x)))
+
+(defun convert-op (op)
+  "Make op confirm to the (EXECUTING op) convention."
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op)) (op-add-list op)))
+  op)
+
+(defun op (action &key preconds add-list del-list)
+  "Make a new operator that obeys the (EXECUTING op) convention."
+  (convert-op
+   (make-op :action action :preconds preconds
+            :add-list add-list :del-list del-list)))
+
+;; We can convert existing operators using convert-op
+(mapc #'convert-op *school-ops*)
+
+;; GPS no longer returns solved when done, but a sequence of actions
+(defvar *ops* nil "A list of available operators.")
+
+(defstruct op "An operation"
+           (action nil) (preconds nil) (add-list nil) (del-list nil))
+
+(defun GPS (state goals &optional (*ops* *ops*))
+  "General Problem Solver: from state, achieve goals using *ops*."
+  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
+
+;; No more global state variable ; solves the 'leap before you look problem'
+;; The program keeps track of the local state variables now
+;; achieve, achieve-all and apply-op all take an extra argument for the current state and return the new stated
+
+(defun achieve-all (state goal goal-stack)
+  "Achieve each goal, and make sure they still hold at the end."
+  (let ((current-state state))
+    (if (and (every #'(lambda (g)
+                        (setf current-state
+                              (achieve current-state g goal-stack)))
+                    goals)
+             (subsetp goals current-state :test #'equal))
+        current-state)))
+
+(defun achieve (state goal goal-stack)
+  "A goal is achieved if it already holds, or if there is an appropriate op for it that is applicable."
+  (dbg-indent :gps (length goal-stack) "Goal: ~a" goal)
+  (cond ((member-equal goal state) state)
+        ((member-equal goal goal-stack) nil)
+        (t (some #'(lambda (op) (apply-op state goal op goal-stack))
+                 (find-all goal *ops* :test #'appropriate-p)))))
+
+
+;; eql tests literal equality, while equal tests 'look-alike' equality
+(defun member-equal (item list)
+  (member item list :test #'equal))
+
+(defun apply-op (state goal op goal-stack)
+  "Return a new, transformed state if op is applicable."
+  (dbg-indent :gps (length goal-stack) "Consider: ~a" (op-action op))
+  (let ((state2 (achieve-all state (op-preconds op)
+                             (cons goal goal-stack))))
+    (unless (null state2)
+      ;; Return an updated state
+      (dgb-indent :gps (length goal-stack) "Action: ~a" (op-action op))
+      (append (remove-if #'(lambda (x)
+                             (member-equal x (op-del-list op)))
+                         state2)
+              (op-add-list op)))))
+
+(defun appropriate-p (goal op)
+  "An op is appropriate to a goal if it is in its add-list."
+  (member-equal goal (op-add-list op)))
+
+;; Function to be used as a declaration that a given list of operators is to be used for a series of problems
+(defun use (oplist)
+  "Use oplist as the default list of operators."
+  ;; Return something useful, but not too verbose
+  ;; the number of operators
+  (length (setf *ops* oplist)))
